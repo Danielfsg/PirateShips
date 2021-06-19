2c41b96b284c21f92453c69c9cbd7645b7af5d97
Daniel G <dfsguer@gmail.com>
2021-06-19 13:04:23 +0100

    Initial dagger hilt setup

---
+++ app/src/main/java/com/danielfsg/pirateships/PirateShipsApplication.kt
@@ -0,0 +1,9 @@
+package com.danielfsg.pirateships
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PirateShipsApplication : Application() {
+
+}
