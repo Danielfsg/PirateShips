4df7042056de472a32886dba2482916db76eac2a
Daniel G <dfsguer@gmail.com>
2021-06-19 14:34:22 +0100

    Created get pirate ships usecase

---
+++ app/src/main/java/com/danielfsg/pirateships/domain/usecase/GetPirateShipsUseCase.kt
@@ -0,0 +1,16 @@
+package com.danielfsg.pirateships.domain.usecase
+
+import com.danielfsg.pirateships.base.SingleUseCase
+import com.danielfsg.pirateships.domain.model.PirateShipEntity
+import com.danielfsg.pirateships.domain.repository.Repository
+import io.reactivex.rxjava3.core.Single
+import javax.inject.Inject
+
+class GetPirateShipsUseCase @Inject constructor(
+    private val repository: Repository
+) : SingleUseCase<List<PirateShipEntity>>() {
+
+    override fun buildUseCaseSingle(): Single<List<PirateShipEntity>> {
+        return repository.getPirateShips()
+    }
+}
